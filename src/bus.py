ed14f20fa003b9601e3ef08c693471e9b1935724
Tom Britton <tom.jf.britton@gmail.com>
2022-08-02 14:04:27 +0100

    Finished lab

---
+++ src/bus.py
@@ -0,0 +1,25 @@
+class Bus():
+    def __init__(self, route_number, destination):
+        self.route_number = route_number
+        self.destination = destination
+        self.passenger = []
+
+    def drive(self):
+        return "Brum brum"
+
+    def passenger_count(self):
+        return len(self.passenger)
+
+    def pick_up(self, passenger):
+        self.passenger.append(passenger)
+
+    def drop_off(self, person):
+        self.passenger.remove(person)
+
+    def empty(self):
+        self.passenger = []
+
+    def pick_up_from_stop(self, bus_stop):
+        for person in bus_stop.queue:
+            self.passenger.append(person)
+
